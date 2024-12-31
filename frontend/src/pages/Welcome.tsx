dd58e4392d2e040c49169f39d54f19c1a573cada
Adhijaan <adnanr@umich.edu>
2024-12-31 17:00:27 -0500

    Updated Welcome Page

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Welcome() {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <>
+      <div>
+        <h1>
+          Meet <span>Llemval</span>
+        </h1>
+        <h3>A simplified way to compare multiple LLMs in context of your needs.</h3>
+      </div>
+      <button>
+        <Link to="/Dashboard">Get Started</Link>
+      </button>
+    </>
   );
 }
