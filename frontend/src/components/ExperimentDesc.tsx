c64cef4e0c22a899bbe97e6e07f00eda21d1c183
Adhijaan <adnanr@umich.edu>
2025-02-01 12:52:10 -0500

    Feat: Build Page wip

--- frontend/src/components/ExperimentDesc.tsx
+++ frontend/src/components/ExperimentDesc.tsx
@@ -1,16 +1,30 @@
 // components/ExperimentDesc.tsx
-import { Card, CardContent, Typography } from "@mui/material";
-import { Experiment } from "../types";
-export default function ExperimentDesc(experiment: Experiment) {
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import { Experiment, TestCase } from "../types";
+
+interface ExperimentDescProps {
+  experiment: Experiment;
+  key: number;
+}
+
+export default function ExperimentDesc({ experiment }: ExperimentDescProps) {
   return (
     <Card sx={{ minWidth: 275, mb: 2 }}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {experiment.name}
-        </Typography>
-        {experiment.testCases?.map((testCase, index) => (
-            
-        ))}
+      <CardContent sx={{ pt: 3, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <Typography variant="h5" component="div">
+            {experiment.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {experiment.id}
+          </Typography>
+        </Box>
+        {/* {experiment.testCases?.map((testCase, index) => (
+          <Typography key={index} variant="body2" color="text.secondary">
+            {testCase.id}
+          </Typography>
+        ))} */}
+        <Button>View</Button>
       </CardContent>
     </Card>
   );
--- frontend/src/pages/Build.tsx
+++ frontend/src/pages/Build.tsx
@@ -1,9 +1,36 @@
 // pages/Build.tsx
-import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Button, Container, Typography } from "@mui/material";
+import { red } from "@mui/material/colors";
+import { Experiment } from "../types";
+import { getExperiments } from "../api";
+import ExperimentDesc from "../components/ExperimentDesc";
 export default function Build() {
+  const [experiments, setExperiments] = useState<Experiment[]>([]);
+  useEffect(() => {
+    document.title = "Build";
+    getExperiments().then((data) => setExperiments(data));
+  }, []);
   return (
-    <div>
-      <h1>Build Page Under Construction</h1>
-    </div>
+    <>
+      <Typography variant="h3" align="center" gutterBottom>
+        Build
+      </Typography>
+      <Typography variant="h6" align="center" gutterBottom sx={{ mb: 4 }}>
+        Define experiements and assign test cases
+      </Typography>
+      <Container maxWidth={false} sx={{ mb: 4, pb: 1, borderColor: red, borderBottom: 1 }}>
+        <Button variant="contained" color="primary" sx={{ mr: 2 }}>
+          Create Experiment
+        </Button>
+        <Button variant="contained" color="primary">
+          Create Test Case
+        </Button>
+      </Container>
+      <Container maxWidth="lg"></Container>
+      {experiments.map((experiment, index) => (
+        <ExperimentDesc key={index} experiment={experiment} />
+      ))}
+    </>
   );
 }
