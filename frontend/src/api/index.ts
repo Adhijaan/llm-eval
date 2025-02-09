155014025d5a595af1cc6529cc538745f4ee6f3e
Adhijaan <adnanr@umich.edu>
2025-02-08 23:23:00 -0500

    Fix: api routes (trailing slash)

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -4,30 +4,30 @@ import { Experiment, TestCase, TestCaseResult } from "../types";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const createTestCase = async (testCase: Omit<TestCase, "id">) => {
-  const response = await axios.post(`${API_BASE_URL}/api/test-cases`, testCase);
+  const response = await axios.post(`${API_BASE_URL}/api/test-cases/`, testCase);
   return response.data;
 };
 
 export const deleteTestCase = async (testCaseId: number) => {
-  await axios.delete(`${API_BASE_URL}/api/test-cases/${testCaseId}`);
+  await axios.delete(`${API_BASE_URL}/api/test-cases/${testCaseId}/`);
 };
 
 export const getExperiments = async () => {
-  const response = await axios.get(`${API_BASE_URL}/api/experiments`);
+  const response = await axios.get(`${API_BASE_URL}/api/experiments/`);
   return response.data;
 };
 
 export const createExperiment = async (experiment: Omit<Experiment, "id">) => {
-  const response = await axios.post(`${API_BASE_URL}/api/experiments`, experiment);
+  const response = await axios.post(`${API_BASE_URL}/api/experiments/`, experiment);
   return response.data;
 };
 
 export const deleteExperiment = async (experimentId: number) => {
-  await axios.delete(`${API_BASE_URL}/api/experiments/${experimentId}`);
+  await axios.delete(`${API_BASE_URL}/api/experiments/${experimentId}/`);
 };
 
 export const runExperiment = async (experimentId: number, model: string) => {
-  const response = await axios.post(`${API_BASE_URL}/api/experiments/${experimentId}/run`, {
+  const response = await axios.post(`${API_BASE_URL}/api/experiments/${experimentId}/run/`, {
     model,
   });
   console.log(response);
@@ -35,6 +35,6 @@ export const runExperiment = async (experimentId: number, model: string) => {
 };
 
 export const getResults = async (): Promise<TestCaseResult[]> => {
-  const response = await axios.get(`${API_BASE_URL}/api/results`);
+  const response = await axios.get(`${API_BASE_URL}/api/results/`);
   return response.data;
 };
